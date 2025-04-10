5d21fa75ffaf63a791ab6874fbcad97bcf2b46c8
Hjörvar Ingi <hih@fb.is>
2025-04-10 14:13:13 +0000

    Create millifaersla.py

---
+++ if/millifaersla.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+monnei = "Monnei"
+fjee = "Fjee"
+dolla = "Dolladollabilljoll"
+
+if a < b and a < c:
+    print(monnei)
+elif b < a and b < c:
+    print(fjee)
+else:
+    print(dolla)
